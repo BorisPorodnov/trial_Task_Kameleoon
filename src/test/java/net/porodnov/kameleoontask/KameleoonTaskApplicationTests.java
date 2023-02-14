ebd2eae4eb47c1de5c13a32f194c54104aecb246
Boris Porodnov <porodnov.b@mail.ru>
2023-02-14 21:56:45 +0700

    first commit of the project

---
+++ src/test/java/net/porodnov/kameleoontask/KameleoonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package net.porodnov.kameleoontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
